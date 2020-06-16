274c00f1bf0067eee6c2eddc654b13d768eac0a0
Teo-Reat <teo.reat@gmail.com>
2020-06-16 20:23:19 +0300

    Minor changes

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,9 @@ $(document).ready(() => {
 	$('#button__consultation').click(() => {
 		$('#popup__consultation').removeClass('hide')
 	})
+	$('#popup__consultation__close').click(() => {
+		$('#popup__consultation').addClass('hide')
+	})
 	$(document).keyup(function(e) {
 		if (e.key === "Escape") {
 			$('#popup__consultation').addClass('hide')
@@ -13,6 +16,20 @@ $(document).ready(() => {
 		$('#main-navigation__system').toggleClass('main-navigation__item__active')
 		$('#main-navigation__system__menu').toggleClass('hide')
 	})
+
+	// let menu = $('.header');
+	// let origOffsetY = menu.offset().top;
+	//
+	// function scroll() {
+	// 	if ($(window).scrollTop() >= origOffsetY) {
+	// 		$('.menu').addClass('sticky');
+	// 		$('.content').addClass('menu-padding');
+	// 	} else {
+	// 		$('.menu').removeClass('sticky');
+	// 		$('.content').removeClass('menu-padding');
+	// 	}
+	// }
+	// document.onscroll = scroll;
 })
 
 

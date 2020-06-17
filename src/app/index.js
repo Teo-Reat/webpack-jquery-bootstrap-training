b4cac09383a1b036801a63c85bbecb7ff41e78c6
Teo-Reat <teo.reat@gmail.com>
2020-06-17 23:22:46 +0300

    Mobile version started

--- src/app/index.js
+++ src/app/index.js
@@ -16,20 +16,6 @@ $(document).ready(() => {
 		$('#main-navigation__system').toggleClass('main-navigation__item__active')
 		$('#main-navigation__system__menu').toggleClass('hide')
 	})
-
-	// let menu = $('.header');
-	// let origOffsetY = menu.offset().top;
-	//
-	// function scroll() {
-	// 	if ($(window).scrollTop() >= origOffsetY) {
-	// 		$('.menu').addClass('sticky');
-	// 		$('.content').addClass('menu-padding');
-	// 	} else {
-	// 		$('.menu').removeClass('sticky');
-	// 		$('.content').removeClass('menu-padding');
-	// 	}
-	// }
-	// document.onscroll = scroll;
 })
 
 

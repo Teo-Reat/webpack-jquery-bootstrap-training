c4b4b8ab511d76730c3b69f2ff7674761d934f17
Teo-Reat <teo.reat@gmail.com>
2020-06-18 19:41:14 +0300

    Restructurize project

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,20 @@
 import $ from 'jquery'
+import './modal'
+import './util'
 
 $(document).ready(() => {
-	$('#button__consultation').click(() => {
-		$('#popup__consultation').removeClass('hide')
-	})
-	$('#popup__consultation__close').click(() => {
-		$('#popup__consultation').addClass('hide')
-	})
-	$(document).keyup(function(e) {
-		if (e.key === "Escape") {
-			$('#popup__consultation').addClass('hide')
-		}
-	});
-	$('#main-navigation__system').click(() => {
-		$('#main-navigation__system').toggleClass('main-navigation__item__active')
-		$('#main-navigation__system__menu').toggleClass('hide')
-	})
+	$('#myModal').modal('toggle')
+	// $('#button__consultation').click(() => {
+	// 	$('#popup__consultation').removeClass('hide')
+	// })
+	// $('#popup__consultation__close').click(() => {
+	// 	$('#popup__consultation').addClass('hide')
+	// })
+	// $(document).keyup(function(e) {
+	// 	if (e.key === "Escape") {
+	// 		$('#popup__consultation').addClass('hide')
+	// 	}
+	// });
 })
 
 

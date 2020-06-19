d69dda2012f20140f822b01bb8c285c6ecaa3cc4
Teo-Reat <teo.reat@gmail.com>
2020-06-19 18:25:02 +0300

    Edits. Font Awesome added

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,14 @@
 import $ from 'jquery'
 import './modal'
 import './util'
+import '@fortawesome/fontawesome-free'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 $(document).ready(() => {
-	$('#myModal').modal('toggle')
-	// $('#button__consultation').click(() => {
-	// 	$('#popup__consultation').removeClass('hide')
-	// })
-	// $('#popup__consultation__close').click(() => {
-	// 	$('#popup__consultation').addClass('hide')
-	// })
-	// $(document).keyup(function(e) {
-	// 	if (e.key === "Escape") {
-	// 		$('#popup__consultation').addClass('hide')
-	// 	}
-	// });
+	$('#myModal').css('display: none')
 })
 
 

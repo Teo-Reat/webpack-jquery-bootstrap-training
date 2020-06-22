00885d4b3cf77c81a7e6b3f4c8dc7998b8afa16a
Teo-Reat <teo.reat@gmail.com>
2020-06-22 13:26:02 +0300

    Mobile version completed

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,28 @@ import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
 $(document).ready(() => {
+	// Modal window
 	$('#myModal').css('display: none')
+
+	// Menu on mobile version
+	$('.mobile-bars').click(() => {
+		$('.mobile-bars').css('display','none')
+		$('.mobile-bars__close').css('display', 'block')
+		$('.main-navigation').slideDown()
+		$('.contact-block').slideDown()
+	})
+	$('.mobile-bars__close').click(() => {
+		$('.mobile-bars__close').css('display','none')
+		$('.mobile-bars').css('display', 'block')
+		$('.main-navigation').slideUp()
+		$('.contact-block').slideUp()
+	})
+
+	// Submenu, mobile version
+	$('#main-navigation__system').click(() => {
+		$('.main-navigation__item__list').slideToggle('slow', 'linear')
+		$('#main-navigation__system').toggleClass('main-navigation__item__active')
+	})
 })
 
 

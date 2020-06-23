54256ad9504e3cd2b4244a8cbd167160c51aa0d6
Teo-Reat <teo.reat@gmail.com>
2020-06-23 17:31:53 +0300

    The FontAwesome is replased by IcoMoon

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,6 @@
 import $ from 'jquery'
 import './modal'
 import './util'
-import '@fortawesome/fontawesome-free'
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
 import 'slick-carousel'
 
 $(document).ready(() => {
